bb2d393d5fc713a9d58991bb8556f1d5ecb01e26
shuixianbing <shui6666@126.com>
2018-06-29 16:39:12 +0800

    add connect

--- app/src/main/java/com/bshui/mizu/MainActivity.java
+++ app/src/main/java/com/bshui/mizu/MainActivity.java
@@ -1,24 +1,42 @@
 package com.bshui.mizu;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
+
+import android.bluetooth.BluetoothGatt;
+
+import android.bluetooth.BluetoothGattServer;
+
+import android.bluetooth.le.BluetoothLeAdvertiser;
+
 import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 import com.bshui.mizu.adapter.DeviceAdapter;
 import com.clj.fastble.BleManager;
+import com.clj.fastble.callback.BleGattCallback;
 import com.clj.fastble.callback.BleScanCallback;
 import com.clj.fastble.data.BleDevice;
+import com.clj.fastble.exception.BleException;
 
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button bt_find;
     private ListView lv_device;
     private DeviceAdapter mDeviceAdapter;
+    private ProgressDialog progressDialog;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,8 +72,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bt_find = (Button)findViewById(R.id.bt_find);
         bt_find.setOnClickListener(this);
         lv_device = (ListView)findViewById(R.id.lv_device);
+        progressDialog = new ProgressDialog(MainActivity.this);
         mDeviceAdapter = new DeviceAdapter(this);
         lv_device.setAdapter(mDeviceAdapter);
+        lv_device.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //点击蓝牙设备选项 连接蓝牙 并进入相应的操作页面
+
+                final BleDevice bleDevice = mDeviceAdapter.getItem(i);
+                if(bleDevice == null)
+                    return;
+
+                if(!BleManager.getInstance().isConnected(bleDevice)){
+                    BleManager.getInstance().cancelScan();
+                    connect(bleDevice);
+
+                }else{
+                    //如果已经连上,直接转到设备信息页面
+                    Intent intent = new Intent(MainActivity.this,DeviceInfoActivity.class);
+                    intent.putExtra("BleDevice",bleDevice);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -88,4 +128,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
     }
+
+    private void connect(final BleDevice bleDevice){
+        //Connect with device
+        BleManager.getInstance().connect(bleDevice, new BleGattCallback() {
+            @Override
+            public void onStartConnect() {
+                //开始连接
+                progressDialog.show();
+
+            }
+
+            @Override
+            public void onConnectFail(BleDevice bleDevice, BleException exception) {
+                //连接失败
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onConnectSuccess(BleDevice bleDevice, BluetoothGatt gatt, int status) {
+                //连接成功
+
+                progressDialog.dismiss();
+                Intent intent = new Intent(MainActivity.this,DeviceInfoActivity.class);
+                intent.putExtra("BleDevice",bleDevice);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onDisConnected(boolean isActiveDisConnected, BleDevice device, BluetoothGatt gatt, int status) {
+                //连接断开
+                progressDialog.dismiss();
+                Log.i("TAG","---onDisConnected");
+            }
+        });
+
+    }
+
+
 }
